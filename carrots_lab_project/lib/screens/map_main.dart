a0b0e90d42e7eceae82217a3a98bfb2e63080506
Reloxed <antoniomma97@gmail.com>
2021-06-12 20:45:59 +0200

    Ubicación actual añadida

--- carrots_lab_project/lib/screens/map_main.dart
+++ carrots_lab_project/lib/screens/map_main.dart
@@ -2,6 +2,7 @@ import 'package:carrots_lab_project/database/queries.dart';
 import 'package:flutter/material.dart';
 import 'package:latlong2/latlong.dart';
 import 'package:flutter_map/flutter_map.dart';
+import 'package:flutter_map_location/flutter_map_location.dart';
 
 class MapMain extends StatefulWidget {
   @override
@@ -11,6 +12,7 @@ class MapMain extends StatefulWidget {
 }
 
 class _MapMainState extends State<MapMain> {
+  final MapController mapController = MapController();
 
   @override
   Widget build(BuildContext context) {
@@ -23,7 +25,12 @@ class _MapMainState extends State<MapMain> {
             );
           }
           return FlutterMap(
+            mapController: mapController,
             options: new MapOptions(
+              interactiveFlags: InteractiveFlag.pinchZoom | InteractiveFlag.drag,
+              plugins: [
+                LocationPlugin(),
+              ],
               center: new LatLng(37.285427171289975, -5.910435014322033),
               zoom: 13.0,
             ),
@@ -32,7 +39,52 @@ class _MapMainState extends State<MapMain> {
                   urlTemplate: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", subdomains: ['a', 'b', 'c']),
               new MarkerLayerOptions(markers: snapshot.requireData),
             ],
+            nonRotatedLayers: <LayerOptions>[
+              // USAGE NOTE 3: Add the options for the plugin
+              LocationOptions(
+                locationButton(),
+                onLocationRequested: (LatLngData? ld) {
+                  if (ld == null) {
+                    return;
+                  }
+                  mapController.move(ld.location, 16.0);
+                },
+              ),
+            ],
           );
         });
+  }
+
+  LocationButtonBuilder locationButton() {
+    return (BuildContext context, ValueNotifier<LocationServiceStatus> status,
+        Function onPressed) {
+      return Align(
+        alignment: Alignment.bottomRight,
+        child: Padding(
+          padding: const EdgeInsets.only(bottom: 16.0, right: 16.0),
+          child: FloatingActionButton(
+              child: ValueListenableBuilder<LocationServiceStatus>(
+                  valueListenable: status,
+                  builder: (BuildContext context, LocationServiceStatus value,
+                      Widget? child) {
+                    switch (value) {
+                      case LocationServiceStatus.disabled:
+                      case LocationServiceStatus.permissionDenied:
+                      case LocationServiceStatus.unsubscribed:
+                        return const Icon(
+                          Icons.location_disabled,
+                          color: Colors.white,
+                        );
+                      default:
+                        return const Icon(
+                          Icons.location_searching,
+                          color: Colors.white,
+                        );
+                    }
+                  }),
+              onPressed: () => onPressed()),
+        ),
+      );
+    };
   }
 }
